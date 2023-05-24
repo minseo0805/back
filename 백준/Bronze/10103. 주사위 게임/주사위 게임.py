ab07f768a8dfa0a3fe4ce077e7847aa4264852fb
minseo0805 <118956076+minseo0805@users.noreply.github.com>
2023-05-24 20:14:18 +0900

    [Bronze III] Title: 주사위 게임, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/10103. 주사위 게임/주사위 게임.py
@@ -0,0 +1,12 @@
+n = int(input())
+x = y = 100
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    if a > b:
+        y -= a
+    elif a < b:
+        x -= b
+        
+print(x)
+print(y)
