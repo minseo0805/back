e728532082586f421365caeb3e7bbff1cd8579c2
minseo0805 <118956076+minseo0805@users.noreply.github.com>
2023-05-24 20:59:08 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,20 @@
+N, M = map(int, input().split()) 
+visited = [0 for _ in range(N)] 
+arr = [] 
+
+def dfs(cnt): 
+    if cnt == M: 
+        print(*arr) 
+        return 
+    for i in range(N): 
+        if visited[i] == 0: 
+            visited[i] = 1 # 중복 제거 
+            arr.append(i+1) 
+            
+            dfs(cnt+1) # 다음 깊이 탐색 
+            arr.pop() # 탐사한 내용 제거 
+            
+            # 순열 부분과의 차이점 
+            for j in range(i+1, N): 
+                visited[j] = 0 
+dfs(0)
