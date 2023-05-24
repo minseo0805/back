39857186adce413ddc1489ed55b96e794ef42c48
minseo0805 <118956076+minseo0805@users.noreply.github.com>
2023-05-24 20:22:45 +0900

    [Bronze III] Title: 수도요금, Time: 76 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Bronze/10707. 수도요금/수도요금.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+p = int(input())
+x = a * p
+if c < p:
+    y = b + ((p - c) * d)
+else:
+    y = b
+if x < y:
+    print(x)
+else:
+    print(y)
